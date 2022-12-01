b4aae2078f57c9ef4c78d332e2bf5badeef0981b
Jigg <stayhidden21@gmail.com>
2022-11-30 23:05:52 -0600

    refactoring

---
+++ market.py
@@ -0,0 +1,38 @@
+import CargoItems
+import csv
+
+class tradeOffer():
+    def __init__(self,id,isSale,p) -> None:
+        '''create a trade object to represent a buy order'''
+        self.item = CargoItems.getItemObj(id)
+        self.cost = 0
+        
+    
+    
+        
+
+class Market():
+    '''an object to handle the market'''
+    def __init__(self) -> None:
+        self.items = []
+        self.agents = []
+
+
+class tradeAgent():
+    '''class for how agents trade with one another'''
+    def __init__(self) -> None:
+        self.gold = 0
+        self.relations = dict()
+        with open('data\\defaultAgents.csv') as csvFile:
+            reader = csv.DictReader(csvFile)
+            for row in reader:
+                row["Name"] = row
+
+def getItemCost() -> int:
+    pass
+
+def sellItem() -> int:
+    pass
+
+def buyItem() -> str:
+    pass
