e8fc9bfa823c07c9fc4650d61ff63e3c5971c865
Jigg <stayhidden21@gmail.com>
2023-03-07 22:38:14 -0600

    started culture system

--- market.py
+++ market.py
@@ -2,32 +2,38 @@ import CargoItems
 import csv
 
 class tradeOffer():
-    def __init__(self,id,isSale,p) -> None:
-        '''create a trade object to represent a buy order'''
-        self.item = CargoItems.getItemObj(id)
-        self.cost = 0    
+    def __init__(self,itemId) -> None:
+        '''create a trade object to represent a buy offer'''
+        self.item = CargoItems.getItemObj(itemId)
+        
 
 class Market():
     '''an object to handle the market, including all the agents'''
     def __init__(self) -> None:
-        self.items = []
+        self.offers = []
         self.agents = []
+        self.itemsBaseLine = dict()
+
+    def newTradeOffer(self,offer):
+        '''accept a new trade offer into the market, and returns the price dictionary'''
+        self.offers.append(offer)
+        for agnt in self.agents:
+            agnt.evaluateOffer(offer)
 
 class tradeAgent():
     '''class for how agents trade with one another'''
     def __init__(self) -> None:
         self.gold = 0
+        #a dictionary of each trade agent : their relationship to each other
         self.relations = dict()
+        #a dictionary of each item : this agent's need for it 
+        self.needs = dict()
+        #get a few 
         with open('data\\defaultAgents.csv') as csvFile:
             reader = csv.DictReader(csvFile)
             for row in reader:
-                row["Name"] = row
+                row["Name"] = 
 
-    def getItemCost() -> int:
-        pass
-
-    def sellItem() -> int:
-        pass
-
-    def buyItem() -> str:
+    def evaluateOffer(self,offer):
+        '''evaluates the '''
         pass
